dc3d7da57bedb7a33914063d0b3b59c714b32350
madhavi569 <madhavireddi27@gmail.com>
2024-10-15 11:04:24 +0530

    Create Count.py

---
+++ Count.py
@@ -0,0 +1,7 @@
+'''
+Write a program to get the tuple values in a single line separated by space and count the nuber of times the given x value is present in the given tuple.
+Sample Input:
+1 2 3 1 2 3 4 1 2 1
+1
+'''
+4
