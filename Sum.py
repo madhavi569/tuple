7399d7c5f8ddedac62388b1d2655c3a51c10a29e
madhavi569 <madhavireddi27@gmail.com>
2024-10-15 11:00:21 +0530

    Create Sum.py

---
+++ Sum.py
@@ -0,0 +1,9 @@
+'''
+Write a program to get n number of tuple elements from the user in separate lines and print the sum of their values using predefined method.
+Sample Input:
+3
+10
+20
+30
+'''
+60
