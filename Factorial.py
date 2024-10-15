4c2aeb37295147f8fab386d4012b1fdef7a09f2b
madhavi569 <madhavireddi27@gmail.com>
2024-10-15 11:07:56 +0530

    Create Factorial.py

---
+++ Factorial.py
@@ -0,0 +1,7 @@
+'''
+ Write the program to count the number of times the given number (x) is present in the given tuple list and print it's factorial value without using factorial() method.
+Sample Input:
+1 2 3 4 1 5 1
+1
+'''
+6
