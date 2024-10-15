4747e6cf3b2ca1fe06f2b08fd128e4952092d833
madhavi569 <madhavireddi27@gmail.com>
2024-10-15 11:06:47 +0530

    Create Odd.py

---
+++ Odd.py
@@ -0,0 +1,6 @@
+'''
+ Raju is a 3rd grade kid, he is struggling to find which is "even" number and which is a "odd" number. So, his teacher gave him a task to find how many even numbers and how many odd numbers are there in the given collection of values and print "Even" if even count is more than odd count, else print "Odd", if odd count is more and print "Equal" if both even count and odd count are same. Help Raju to understand the difference of even and odd.
+Sample Input:
+1 2 3 4 5
+'''
+Odd
